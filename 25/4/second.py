d939b0f5501c17738862591d7517de8ec56a9940
Štěpán Zelenka <stepan.zelenka@orbi.cz>
2025-12-05 08:51:03 +0100

    day 4, second part

---
+++ 25/4/second.py
@@ -0,0 +1,62 @@
+text = open('input', 'r')
+
+grid = []
+while True:
+    line = text.readline()[:-1]
+    if not line:
+        break
+
+    grid.append([roll for roll in line])
+
+
+def is_outer_roll(y_new, x_new):
+    if y_new not in range(len(grid)) or x_new not in range(len(grid[0])):
+        return False
+
+    point = grid[y_new][x_new]
+
+    if point == '@':
+        return True
+
+    return False
+
+
+next_grid = grid.copy()
+next_removed_rolls_count = 999
+total_removed_rolls_count = 0
+while next_removed_rolls_count > 0:
+    accessible_rolls_count = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] != '@':
+                continue
+
+            offsets = [
+                (-1, -1),
+                (0, -1),
+                (1, -1),
+                (-1, 0),
+                (1, 0),
+                (-1, 1),
+                (0, 1),
+                (1, 1),
+            ]
+            adjacent_rolls_count = 0
+            for offset in offsets:
+                y_offset, x_offset = offset
+                if is_outer_roll(y + y_offset, x + x_offset):
+                    adjacent_rolls_count += 1
+
+            if adjacent_rolls_count < 4: # remove
+                accessible_rolls_count += 1
+                next_grid[y][x] = '.'
+
+    total_removed_rolls_count += accessible_rolls_count
+    next_removed_rolls_count = accessible_rolls_count # reset the count
+    grid = next_grid.copy()
+
+
+print(total_removed_rolls_count)
+
+
+
